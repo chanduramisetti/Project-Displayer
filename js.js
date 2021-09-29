a59b006b72204048f3e1e9d786ee359f0cc08309
chanduramisetti <86049039+chanduramisetti@users.noreply.github.com>
2021-09-29 09:10:35 +0530

    Add files via upload
    These are the codes for the Webpage that I've designed to have look of it add all these files to a folder and open in it in visual code and open it in Chrome

---
+++ js.js
@@ -0,0 +1,42 @@
+//project 1
+let b1=document.querySelector('#po1');
+let content=document.querySelector('#c1');
+b1.addEventListener('click',()=>{
+    content.innerHTML='\
+    <object id="obj1" data="base1.html"></object>\
+    ';
+    
+})
+//Home
+let bh=document.querySelector('#Home');
+let content1=document.querySelector('#c1');
+bh.addEventListener('click',()=>{
+    content1.innerHTML='\
+    <object id="obj1" data="Home.html"></object>\
+    ';
+    
+})
+let b2=document.querySelector('#po2');
+let content2=document.querySelector('#c1');
+b2.addEventListener('click',()=>{
+    content2.innerHTML='\
+    <object id="obj1" data="base2.html"></object>\
+    ';
+    
+})
+let b3=document.querySelector('#po3');
+let content3=document.querySelector('#c1');
+b3.addEventListener('click',()=>{
+    content3.innerHTML='\
+    <object id="obj1" data="base3.html"></object>\
+    ';
+    
+})
+let b4=document.querySelector('#Me');
+let content4=document.querySelector('#c1');
+b4.addEventListener('click',()=>{
+    content4.innerHTML='\
+    <object id="obj1" data="base4.html"></object>\
+    ';
+    
+})
